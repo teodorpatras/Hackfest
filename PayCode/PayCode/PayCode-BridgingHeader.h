3072245d7fc9ca414eed8b6edf34d59c3db82719
Teodor Patras <teodor.patras@gmail.com>
2015-09-26 14:58:07 +0200

    Initial workspace

---
+++ PayCode/PayCode/PayCode-BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+//  PayCode-BridgingHeader.h
+//  PayCode
+//
+//  Created by Teodor Patras on 26/09/15.
+//  Copyright © 2015 Teodor Patras. All rights reserved.
+//
+
+#import <SVProgressHUD/SVProgressHUD.h>
+
+
+#import "CardIO.h"
+
+@import AudioToolbox;
+@import AVFoundation;
+@import CoreMedia;
+@import CoreVideo;
+@import MobileCoreServices;
---
+++ PayCode/Pods/Target Support Files/Pods-PayCode/Pods-PayCode-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PayCodeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PayCodeVersionString[];
+
